d9f5f293449689611620aa36ea878a4433123867
ZhaoZ <zhaoz@gilmour.org>
2017-09-29 14:48:15 -0400

    cut turn_test

--- student.py
+++ student.py
@@ -21,9 +21,9 @@ class Piggy(pigo.Pigo):
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
         self.STOP_DIST = 30
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 140
+        self.LEFT_SPEED = 120
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.RIGHT_SPEED = 140
+        self.RIGHT_SPEED = 120
         # This one isn't capitalized because it changes during runtime, the others don't
         self.turn_track = 0
         # Our scan list! The index will be the degree and it will store distance
@@ -75,7 +75,7 @@ class Piggy(pigo.Pigo):
 
 
     def cha_cha(self):
-        for x in range(3):
+        for x in range(6):
             self.encR(4)
             self.encL(-4)
             self.encR(-4)
