a3f9a216215300a13cc8fcf6b6975f7d28f2f028
ZhaoZ <zhaoz@gilmour.org>
2017-10-18 14:41:32 -0400

    improve scan_count method

--- student.py
+++ student.py
@@ -93,7 +93,7 @@ class Piggy(pigo.Pigo):
 
     def obstacle_count(self):
         """scans and estimates the number of obstacles within sight"""
-        self.wide_scan()
+        self.wide_scan(20)
         found_something = False
         counter = 0
         for distance in self.scan:
