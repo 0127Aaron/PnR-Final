31d8dea85e1e2d2fe7af153484d1af845da3b4b2
ZhaoZ <zhaoz@gilmour.org>
2017-10-20 14:15:29 -0400

    New obstacle_check method

--- student.py
+++ student.py
@@ -92,19 +92,19 @@ class Piggy(pigo.Pigo):
         return True      # continue the dance method
 
     def obstacle_count(self):
+        self.set_speed(10, 10)
+        self.encR(30)
         """scans and estimates the number of obstacles within sight"""
-        self.wide_scan(2)
+        self.flush_scan()
         found_something = False
         counter = 0
         for distance in self.scan:
             if distance and distance < 70 and not found_something:
-                found_something = True
-                counter += 1
-                print("Object #%d found, I think" % counter)
+                    found_something = True
+                    counter += 1
+                    print("Object #%d found, I think" % counter)
             if distance and distance > 70 and found_something:
-                found_something = False
-
-
+                    found_something = False
         print("\n----I SEE %d OBJECT----\n" % counter)
 
     def head_fwd(self):     # make the sensor forward
