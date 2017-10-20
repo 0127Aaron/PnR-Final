b819cc796791eb3662305416bf55abe83383144d
ZhaoZ <zhaoz@gilmour.org>
2017-10-20 15:07:49 -0400

    Old version for obstacle

--- student.py
+++ student.py
@@ -96,7 +96,7 @@ class Piggy(pigo.Pigo):
         count = 0
         for x in range(4):
             count += self.obstacle_count()
-            self.encR(9)
+            self.encR(8)
         print("\n----There are totally %d objects----\n" % count)
 
     def obstacle_count(self):
