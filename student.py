22b4188f2348691448f82596a1f0fab51860395a
ZhaoZ <zhaoz@gilmour.org>
2017-11-29 14:47:06 -0500

    add sleep in restore heading

--- student.py
+++ student.py
@@ -204,7 +204,7 @@ class Piggy(pigo.Pigo):
                 largest_dist = distance
                 ang = index
         print("I think the best angle is %d\n" % ang)
-        turn = int(7.1 * abs(ang - self.MIDPOINT) / 90)   ##calculate how much it should turn to the valid direction.
+        turn = int(7 * abs(ang - self.MIDPOINT) / 90)   ##calculate how much it should turn to the valid direction.
         if ang <= self.MIDPOINT:
             print("Turn right for %d\n" % turn)
             self. encR(turn)
